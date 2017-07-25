1f2571bcd5a4cf2db7be3a98ce07bef2c660152d
zhaojunlike <zhaojunlike@gmail.com>
2017-07-25 17:28:46 +0800

    '完善'

--- app/alipay_auto_pay/libs/logger.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by zhaojunlike on 7/18/2017.
- */
-const log4js = require('log4js');
-const logger = log4js.getLogger();
-logger.level = 'debug';
-logger.debug("Logger Init Success");
-exports.logger = logger;
--- app/tools/update_host_timezone.sh
+++ app/tools/update_host_timezone.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo su
-sudo echo 'Asia/Shanghai' > /etc/timezone
 docker run --rm -v /etc/localtime:/data/localtime centos cp /usr/share/zoneinfo/Asia/Shanghai /data/localtime
