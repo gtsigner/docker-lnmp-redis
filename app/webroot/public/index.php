e621e4833123bff1ccd8c793aa287a215a76c8e9
zhaojunlike <zhaojunlike@gmail.com>
2017-09-04 08:44:47 +0800

    update

--- app/webroot/public/index.php
+++ app/webroot/public/index.php
@@ -1,5 +1,9 @@
 <?php
 //phpinfo();
+
+xdebug_start_code_coverage();
 define('APP_PATH', "./../");
 require APP_PATH . "lib/Hello.class.php";
 Hello::sayHello();
+var_dump(xdebug_get_code_coverage());
+xdebug_stop_code_coverage();
--- docs/alias.sh
+++ docs/alias.sh
@@ -4,3 +4,4 @@ alias composer="docker run -it --rm -v=\$PWD:/app --workdir=/app composer"
 #php
 alias php="docker run -it --rm -v=\$PWD:/app --workdir=/app php:7.1-cli"
 #其他
+alias go="docker run -it --rm -v=\$PWD:/app --workdir=/app golang:1.9.0-stretch go"
